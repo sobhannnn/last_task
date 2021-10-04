05fd26c27fc07339b88c752f645eea7dd32ad5fd
sobhannnn <sobhanr175@gmail.com>
2021-10-04 16:38:15 +0330

    test mutation and query

--- location/admin.py
+++ location/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import LocationUser
 
-admin.site.register(LocationUser)
+class LocationUserAdmin(admin.ModelAdmin):
+    readonly_fields = ['location_lat', 'location_long', 'location_name', 'is_finished', 'start', 'end', 'user',]
+
+
+admin.site.register(LocationUser, LocationUserAdmin)
